948173c6534a6053aa21c524f86d56588a483a50
MKA <mka@step.rivne>
2025-06-04 20:57:44 +0300

    .

--- main.js
+++ main.js
@@ -1,40 +1,82 @@
-const addBtn = document.getElementById("addBtn");
-const tableBody = document.getElementById("product-list");
-const clearBtn = document.getElementById("clearAllBtn");
+// const addBtn = document.getElementById("addBtn");
+const tableBody = document.getElementById("product-list2");
+// const clearBtn = document.getElementById("clearAllBtn");
 
-const form = document.forms.productForm;
-const modelInput = form.model;
-const yearInput = form.year;
-const colorsInput = form.colors;
-const isElectroInput = form.electro;
+// const form = document.forms.productForm;
+// const modelInput = form.model;
+// const yearInput = form.year;
+// const colorsInput = form.colors;
+// const isElectroInput = form.electro;
 
-function ValidItems(){
-    if (modelInput.value === "" || !yearInput.value){
-        return false
-    }
-    return true
+// function ValidItems(){
+//     if (modelInput.value === "" || !yearInput.value){
+//         return false
+//     }
+//     return true
+// }
+
+// addBtn.onclick = (event) => {
+//     event.preventDefault();
+
+//     if(!ValidItems()){
+//         alert("txt box is null")
+//         return;
+//     }
+
+
+//     // read form values
+//     const item = new car(
+//         modelInput.value,
+//         yearInput.value,
+//         colorsInput.value,
+//         isElectroInput.checked
+//     );
+
+//     tableBody.innerHTML += item.toHTMLRow();
+// }
+
+// clearBtn.onclick = () => {
+//     tableBody.innerHTML = "";
+// }
+const fish = new product(
+    "fish",
+    "./images/Food_C205-128.png"
+)
+const egg = new product(
+    "egg",
+    "./images/Food_C203-128.png"
+)
+const a = new product(
+    "cheese",
+    "./images/Food_C217-128.png"
+)
+const b = new product(
+    "meat",
+    "./images/Food_C225-128.png"
+)
+tableBody.innerHTML += fish.toHTMLRow();
+tableBody.innerHTML += egg.toHTMLRow();
+tableBody.innerHTML += a.toHTMLRow();
+tableBody.innerHTML += b.toHTMLRow();
+const addFishBtn = document.getElementById("AddFish");
+const addCheeseBtn = document.getElementById("AddCheese");
+const addEggBtn = document.getElementById("AddEgg");
+const resetBtn = document.getElementById("reset")
+resetBtn.onclick = (event) => {
+    tableBody.innerHTML = ""
 }
-
-addBtn.onclick = (event) => {
-
-    if(!ValidItems()){
-        alert("txt box is null")
-        return;
-    }
-
+addFishBtn.onclick = (event) =>{
     event.preventDefault();
+    tableBody.innerHTML += fish.toHTMLRow();
 
-    // read form values
-    const item = new car(
-        modelInput.value,
-        yearInput.value,
-        colorsInput.value,
-        isElectroInput.checked
-    );
-
-    tableBody.innerHTML += item.toHTMLRow();
 }
+addCheeseBtn.onclick = (event) =>{
+    event.preventDefault();
+    tableBody.innerHTML += a.toHTMLRow();
 
-clearBtn.onclick = () => {
-    tableBody.innerHTML = "";
+}
+addEggBtn.onclick = (event) =>{
+    event.preventDefault();
+    tableBody.innerHTML += egg.toHTMLRow();
+
 }
--- product.js
+++ product.js
@@ -1,26 +1,39 @@
-class car {
-    constructor(model, year, color, electro) {
-        this.model = model;
-        this.year = year;
-        this.color = color;
-        this.electro = electro;
-    }
+// class car {
+//     constructor(model, year, color, electro) {
+//         this.model = model;
+//         this.year = year;
+//         this.color = color;
+//         this.electro = electro;
+//     }
 
-    get electroStatus() {
-        return this.isUsed ? 'Yes' : 'No';
-    }
+//     get electroStatus() {
+//         return this.electro ? 'Yes' : 'No';
+//     }
 
+//     toHTMLRow() {
+//         return `<tr>
+//                     <td>${this.model}</td>
+//                     <td>${this.year}</td>
+//                     <td>${capitalizeFirstLetter(this.color)}</td>
+//                     <td>${this.electroStatus}</td>
+//                 </tr>`;
+//     }
+// }
+
+// function capitalizeFirstLetter(str) {
+//     if (!str) return '';
+//     return str.charAt(0).toUpperCase() + str.slice(1);
+// }
+
+class product{
+    constructor(name,picture){
+        this.name = name
+        this.picture = picture
+    }
     toHTMLRow() {
-        return `<tr>
-                    <td>${this.model}</td>
-                    <td>${this.year}</td>
-                    <td>${capitalizeFirstLetter(this.color)}</td>
-                    <td>${this.electro}</td>
-                </tr>`;
-    }
-}
-
-function capitalizeFirstLetter(str) {
-    if (!str) return '';
-    return str.charAt(0).toUpperCase() + str.slice(1);
+                return `<tr>
+                            <td><img src="${this.picture}" width="50%" height="70%" /></td>
+                            <td>${this.name}</td>
+                        </tr>`;
+            }
 }
